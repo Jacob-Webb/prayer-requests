f888887d49138bb755a767416f54d48644e269d2
Jacob Webb <jacob.webb@rockchurch.com>
2018-07-12 05:32:19 -0700

    nothing really to commit

--- httpdocs/admin/delete_prayers.php
+++ httpdocs/admin/delete_prayers.php
@@ -5,9 +5,12 @@
 require_once('../php/server_info.php');
 $checked_prayers = $_POST['checked_prayer_array'];
 
+//format array so that it can be input as a mysql command
 $delete_query = 'DELETE FROM web_form WHERE hash IN ( ' . implode( ',', $checked_prayers ) . ' );';
 
 $delete_result = $mysqli->query($delete_query) or die ("Query failed: " . $mysqli->error. " Actual query: " . $delete_query);
+
+//send response back to the jQuery that sent the request
 $return_response = "Deleted " . count($checked_prayers) . " prayers";
 echo json_encode($return_response);
 
